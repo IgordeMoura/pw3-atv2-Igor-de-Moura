3105ea204de8b8a525e446c0664bca0a16d6fbb5
IgordeMoura <igormoura14@yahoo.com.br>
2024-03-17 14:50:26 -0300

    atividade finalizada - igor

--- atv2-igor/src/App.js
+++ atv2-igor/src/App.js
@@ -3,10 +3,8 @@ import StudentForm from './Components/StudentForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='container'>
         <StudentForm/>
-      </header>
     </div>
   );
 }
--- atv2-igor/src/Components/StudentForm.js
+++ atv2-igor/src/Components/StudentForm.js
@@ -12,74 +12,77 @@ function StudentForm(){
 
 
     return(
-        <div className='card_form'>
-            <form onSubmit={StudentForm} className='form'>
-                
-                <h1>FORMULÁRIO DE CADASTRO ALUNO</h1>
+        <div className='containerForm'>
+            <div className='card_form'>
+                <form onSubmit={StudentForm} className='form'>
+                    <h1>FORMULÁRIO DE CADASTRO ALUNO</h1>
+                    <label>Digite o Seu Nome Aqui</label>
+                    <input
+                        type='text'
+                        value={name}
+                        onChange={(event)=>{setName(event.target.value)}}
+                        required
+                    />
 
-                <label>Digite o Seu Nome Aqui</label>
-                <input
-                    type='text'
-                    value={name}
-                    onChange={(event)=>{setName(event.target.value)}}
-                    required
-                />
+                    <label>Digite o Seu CPF Aqui</label>
+                    <InputMask
+                        value={cpf}
+                        onChange={(event)=>{setCpf(event.target.value)}}
+                        required
+                        mask='999.999.999-99'
+                    />
 
-                <label>Digite o Seu CPF Aqui</label>
-                <InputMask
-                    value={cpf}
-                    onChange={(event)=>{setCpf(event.target.value)}}
-                    required
-                    mask='999.999.999-99'
-                />
+                    <label>Digite o Seu RG Aqui</label>
+                    <InputMask
+                        value={rg}
+                        onChange={(event)=>{setRg(event.target.value)}}
+                        required                    
+                        mask='99.999.999-9'
+                    />
 
-                <label>Digite o Seu RG Aqui</label>
-                <input
-                    value={rg}
-                    onChange={(event)=>{setRg(event.target.value)}}
-                    required                    
-                    mask='(99) 99999-9999'
-                />
+                    <label>Digite o Seu Telefone Aqui</label>
+                    <InputMask
+                        type='tel'
+                        value={telephoneStudent}
+                        onChange={(event)=>{setTelephoneStudent(event.target.value)}}
+                        required
+                        mask='(99) 99999-9999'
+                    />
 
-                <label>Digite o Seu Telefone Aqui</label>
-                <InputMask
-                    type='tel'
-                    value={telephoneStudent}
-                    onChange={(event)=>{setTelephoneStudent(event.target.value)}}
-                    required
-                    mask='(99) 99999-9999'
-                />
+                    <label>Digite o Telefone Do Seu Responsavel Aqui</label>
+                    <InputMask
+                        value={telephoneResponsible}
+                        onChange={(event)=>{setTelephoneResponsible(event.target.value)}}
+                        required
+                        mask='(99) 99999-9999'
+                    />
 
-                <label>Digite o Telefone Do Seu Responsavel Aqui</label>
-                <InputMask
-                    value={telephoneResponsible}
-                    onChange={(event)=>{setTelephoneResponsible(event.target.value)}}
-                    required
-                    mask='(99) 99999-9999'
-                />
-
-                <label>Digite o Seu Melhor Email Aqui</label>
-                <input
-                    type='email'
-                    value={email}
-                    onChange={(event)=>{setEmail(event.target.value)}}
-                    required
-                />
+                    <label>Digite o Seu Melhor Email Aqui</label>
+                    <input
+                        type='email'
+                        value={email}
+                        onChange={(event)=>{setEmail(event.target.value)}}
+                        required
+                    />
 
 
-                <label>Digite Sua Data De Nascimento Aqui</label>
-                <input
-                    id='teste'
-                    type='date'
-                    value={dateOfBirth}
-                    onChange={(event)=>{setDateOfBirth(event.target.value)}}
-                    required
-                />
+                    <label>Digite Sua Data De Nascimento Aqui</label>
+                    <input
+                        id='teste'
+                        type='date'
+                        value={dateOfBirth}
+                        onChange={(event)=>{setDateOfBirth(event.target.value)}}
+                        required
+                        min='1950-01-01'
+                        max='2024-12-31'
+                    />
 
-                <button  type='submit'>CADASTRAR</button>
-                
-            </ form>
-        </div>
+                    <button type='submit'>CADASTRAR</button>
+                    
+                </ form>
+            </div>
+            <p>Ja é cadastrado? Faça seu Login Aqui</p>
+        </div> 
     );
 }
 
